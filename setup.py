407b4c1214fec6e7620ebd716aeb69368218a2d4
Kivindu <manusha.rajamanukula@gmail.com>
2026-03-18 12:06:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="medical_chatbot",
+  version="0.1.0",
+  author="Kivindu Manusha",
+  author_email="manusha.rajamanukula@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
